8d5f1d01c791034a00fea5f61d7364093cd8599e
Adam Chalkley <atc0005@users.noreply.github.com>
2022-09-03 06:28:17 -0500

    Add missing cmd doc file
    Add "package" doc file to resolve revive `package-comment`
    linting error surfaced by recent linter update.

---
+++ cmd/check_illiad_emails/doc.go
@@ -0,0 +1,18 @@
+// Copyright 2020 Adam Chalkley
+//
+// https://github.com/atc0005/check-illiad
+// Licensed under the MIT License. See LICENSE file in the project root for
+// full license information.
+
+// A Nagios plugin named check_illiad_emails that may be used to monitor email
+// notifications for an ILLiad instance.
+//
+// See our [GitHub repo]:
+//
+//   - to review documentation (including examples)
+//   - for the latest code
+//   - to file an issue or submit improvements for review and potential
+//     inclusion into the project
+//
+// [GitHub repo]: https://github.com/atc0005/check-illiad
+package main
